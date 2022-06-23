3c1daac87a6549c73bab04cfa8ddcbf8dfaf50fc
jwyatt1995@gmail.com <jwyatt1995@gmail.com>
2022-06-23 23:43:20 +0100

    Fix build

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import Release from "./components/Release";
 import axios from "axios";
 import "./App.css";
